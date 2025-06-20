96d28fb411d12aab402ed551d487c8372687da05
karthikabalasubramani75@gmail.com <karthikabalasubramani75@gmail.com>
2025-06-20 14:10:59 +0530

    complete script.js

--- script.js
+++ script.js
@@ -8,11 +8,25 @@ let newObject={
 newObject.display();
 
 console.log("Hello Js");
-console.warn("This is a warning");
-console.error("something went wrong");
+
 console.table([{name:"Alice"},{name:"Bob"}]);
 console.time("loop");
 
 for(let i=0;i<1000000;i++){}
 console.timeEnd("loop");
+//strict mode
+"use strict";
+let a=10;
+b=20;
+//greatest value
+let greatestValue=(1>2)?1:2;
+console.log(greatestValue);
+//&& operator
+let c =(1>2)&&5;
+//|| default operator
+let input=3;
+let d=input||4;
+console.log(b);
 
+//falsy values
+//0'Nan undefined null
