36c7fa1a78e11d6345fcd6bb48ac697f08f28008
karthikabalasubramani75@gmail.com <karthikabalasubramani75@gmail.com>
2025-06-21 09:40:17 +0530

    add cart quantity update

--- cart-quantity/script.js
+++ cart-quantity/script.js
@@ -3,20 +3,56 @@
 
 
 let cartQuantity = Number(localStorage.getItem("cartQuantity"));
-if(!cartQuantity){
-    localStorage.setItem("cartQuantity",0);
-    cartQuantity = 0;
+let cartQuantity = Number(localStorage.getItem("cartQuantity"));
+
+if (!cartQuantity) {
+  localStorage.setItem("cartQuantity", 0);
+  cartQuantity = 0;
 }
-//cartQuantity = cartQuantity || 0;
-function updateQuantity(value){
-    cartQuantity = cartQuantity + value;
-    localStorage.setItem("cartQuantity",cartQuantity);
+// cartQuantity = cartQuantity || 0;
+
+const displayButton = document.getElementById("display-button");
+const plusOneButton = document.getElementById("plus-one");
+const plusTwoButton = document.getElementById("plus-two");
+const plusThreeButton = document.getElementById("plus-three");
+const resetButton = document.getElementById("reset");
+
+displayButton.addEventListener("click", () => {
+  displayQuantity();
+});
+plusOneButton.addEventListener("click", () => {
+  updateQuantity(1);
+  displayQuantity();
+});
+plusTwoButton.addEventListener("click", () => {
+  updateQuantity(2);
+  displayQuantity();
+});
+plusTwoButton.addEventListener("click", () => {
+  updateQuantity(3);
+  displayQuantity();
+});
+resetButton.addEventListener('click', ()=> {
+  resetCart();
+  displayQuantity();
+})
+
+function updateQuantity(value) {
+  cartQuantity = cartQuantity + value;
+  localStorage.setItem("cartQuantity", cartQuantity);
 }
-function displayQuantity(){
-    console.log(`Cart: ${cartQuantity}`);
+
+function displayQuantity() {
+  console.log(`Cart: ${cartQuantity}`);
 }
-function resetCart(){
-    cartQuantity = 0;
-    localStorage.setItem("cartQuantity",cartQuantity);
-    console.log("The cart has been reset");
+
+function resetCart() {
+  cartQuantity = 0;
+  localStorage.setItem("cartQuantity", cartQuantity);
+  console.log("The cart has been reset");
 }
+
+// camelCase
+// PascalCase
+// kebab-case
+// snake_case
